52cc8a0db90b312906477a4550fe86cfc751fa75
esBrunoL <brunolobo.14@hotmail.com>
2025-09-29 12:53:23 -0400

    This is my initial commit

---
+++ hello.py
@@ -0,0 +1,7 @@
+# This script will ask for the user's name and greet them.
+
+# Variables in python dont need to be declared before use.
+name = input("What's your name? ")
+
+# Output a greeting message
+print("Hello, " + name + "!")
